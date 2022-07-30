533b1fdb662a81ad6dba0a86e2dfcea2ce384f89
Prince <princek2530@gmail.com>
2022-07-30 11:11:23 +0530

    branch1 commit

--- src/com/bridgelabz/basics/HelloWorld.java
+++ src/com/bridgelabz/basics/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello Prince");
         System.out.println("How are you");
+        System.out.println("branch1 code");
     }
 }
